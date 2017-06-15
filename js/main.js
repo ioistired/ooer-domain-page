04fe744171054d8a1a4c7de671cde9729cda0f70
ArcticEngie <arceng-Github@yahoo.com>
2017-06-15 13:59:43 +0100

    Adding JS to fix jank :hover

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+var last = document.getElementById("last");
+var container = document.getElementById("container")
 
+last.onmouseover = function(e)
+{
+  last.style.borderColor = "blue";
+  last.style.transition = "transform 0.8s"
+  last.style.transform = "translate(90px, 0px)";
+}
+
+container.onmouseleave = function(e)
+{
+  last.style.borderColor = "red";
+  last.style.transition = "transform 0.8s"
+  last.style.transform = "translate(0px, 0px)";
+}
