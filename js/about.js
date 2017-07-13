711b0c235b805afd0a035f6b02fafc94b2316924
ArcticEngie <arceng-Github@yahoo.com>
2017-07-13 03:14:26 +0100

    Adding JS, no video yet

--- js/about.js
+++ js/about.js
@@ -1,1 +1,23 @@
+var pause = document.getElementById("pause");
+var play = document.getElementById("play");
 
+function endstatic()
+{
+  body.style.background-image = "none";
+}
+
+function startvid()
+{
+  play.style.visibility = "hidden";
+}
+
+function onplay(e)
+{
+  pause.style.visibility = "hidden";
+  play.style.visibility = "visible";
+
+  setTimeout(startvid, 5000);
+  setTimeout(endstatic, 2000);
+}
+
+pause.onclick = onplay;
