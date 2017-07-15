d6f598fae3cdf3a83eff57b265a87ca37468430e
ArcticEngie <arceng-Github@yahoo.com>
2017-07-15 02:51:11 +0100

    Mobile device debugging

--- js/about.js
+++ js/about.js
@@ -84,7 +84,7 @@ window.onload = function()
   staticsound.play();
 
   video = document.createElement("video");
-  video.src = "/Testing%20video.mp4";
+  video.src = "/Testing video.mp4";
   video.pause();
   video.addEventListener('play',function(){
         videoloop();
