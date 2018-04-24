2ac31109219746d807c716643bc5dbaffc5a4280
ArcticEngie <28236173+ArcticEngie@users.noreply.github.com>
2018-04-24 05:48:22 +0100

    Update selector.js

--- gaems/selector.js
+++ gaems/selector.js
@@ -1,5 +1,16 @@
-var text = "Click Start button!! Нажмите кнопку Пуск! Cliquez sur le bouton Démarrer! Klicken den Startknopf!";
+var text = ["Click Start button!", "Нажмите кнопку Пуск!", "Нажмите кнопку Пуск!", "Klicken den Startknopf!"];
+var counter = 0;
+var elem = document.getElementById("wavetext");
+var inst = setInterval(change, 4000);
 
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
 for(var i in text) { 
   if(text[i] === " ") {
     $(".wavetext").append( $("<span>").html("&nbsp;") ); 
