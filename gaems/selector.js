771f78f6d70426ea53e61a7d2d20d08c6c493bbc
ArcticEngie <28236173+ArcticEngie@users.noreply.github.com>
2018-04-24 05:52:06 +0100

    Until can hotswap

--- gaems/selector.js
+++ gaems/selector.js
@@ -1,16 +1,5 @@
-var text = ["Click Start button!", "Нажмите кнопку Пуск!", "Нажмите кнопку Пуск!", "Klicken den Startknopf!"];
-var counter = 0;
-var elem = document.getElementById("wavetext");
-var inst = setInterval(change, 4000);
+var text = "Start! Начало! Début! Anfang!";
 
-function change() {
-  elem.innerHTML = text[counter];
-  counter++;
-  if (counter >= text.length) {
-    counter = 0;
-    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
-  }
-}
 for(var i in text) { 
   if(text[i] === " ") {
     $(".wavetext").append( $("<span>").html("&nbsp;") ); 
