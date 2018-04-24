26fa0fabc7ea2c2fa187deafc918df3ab0f32db8
ArcticEngie <28236173+ArcticEngie@users.noreply.github.com>
2018-04-24 05:30:27 +0100

    Page creation

---
+++ gaems/selector.js
@@ -0,0 +1,9 @@
+var text = "Start! Начало! Début! Anfang!";
+
+for(var i in text) { 
+  if(text[i] === " ") {
+    $(".wavetext").append( $("<span>").html("&nbsp;") ); 
+  } else {  
+    $(".wavetext").append( $("<span>").text(text[i]) ); 
+  }
+}
