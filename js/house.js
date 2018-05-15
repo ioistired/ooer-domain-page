130eeee4e9178a980590c64639c8e30dfbe3355b
ArcticEngie <28236173+ArcticEngie@users.noreply.github.com>
2018-05-15 18:42:38 +0100

    Update house.js

--- js/house.js
+++ js/house.js
@@ -100,8 +100,8 @@ window.onload = function()
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
 
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerWidth;
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
 
 	context.clearRect(0, 0, canvas.width, canvas.height);
 }
